d259a0f524f7a4958f9e2b1e9c3780bdcd9f3da5
Lokesh100997 <l.ashokrao.devghare@accenture.com>
2024-08-22 15:10:18 +0530

    Add new file

---
+++ second_code.py
@@ -0,0 +1,18 @@
+friends = ["Apple","Orange", 5, 345.06, False, "Akash", "Rohan"]
+
+print(friends)
+friends.append("lokesh")
+
+print(friends)
+
+
+l1 = [1, 34, 62, 2, 6, 11]
+
+#l1.sort()               #to sort the list
+#l1.reverse()              #to reverse 
+
+#l1.insert(2, 3333)    #Insert 3333 such that its index is the list is 3
+
+#value = l1.pop(3)         #pop will use to remove the value 
+#print(value)
+print(l1.remove(6))          #remove particular element from list
